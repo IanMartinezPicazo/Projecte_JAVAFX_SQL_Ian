e984c0335c8ec7cbd8147a92354c7c3b52c7fe7b
IanMartinezPicazo <iama249@vidalibarraquer.net>
2025-03-10 10:53:36 +0100

    Avanços documentació

---
+++ Documentació Projecte JAVAFX + SQL - Ian Martínez Picazo/JS/navegacio.js
@@ -0,0 +1,22 @@
+// Script per navegar dinamicament a altres págines.
+
+function navegacioMenu() {
+    /*
+        Obté tots els botons de navegació i els assigna un escoltador d'esdeveniments.
+        Al fer clic en un botó, redirecciona a l'usuari a la pàgina corresponent.
+    */
+    const pagines = document.querySelectorAll("main div");
+    pagines.forEach((pagina, index) => {
+        pagina.addEventListener("click", () => {
+            if (index != 2){
+                window.location.href = `./${pagina.id}.html`;
+            }else{
+                window.open("./Manual d'ús de l'aplicació.pdf", '_blank');
+            }
+        });
+    });
+}
+
+function navegacioBaseDades() {
+    const botó = document.getElementById("menu");
+}
